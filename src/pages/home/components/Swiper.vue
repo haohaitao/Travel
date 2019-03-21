b6c7801a0fbfe21594888a0dd8472b70c26b343b
haohaitao <596336501@qq.com>
2019-03-21 19:38:15 +0800

    swiper

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,50 @@
+<template>
+<div class="wrapper">
+    <swiper :options="swiperOption" >
+        <!-- slides -->
+        <swiper-slide v-for="item of swiperList" :key="item.id">
+            <img class="swiper-img" :src="item.imgurl"/>
+        </swiper-slide>
+        <!-- Optional controls -->
+        <div class="swiper-pagination"  slot="pagination"></div>
+        <div class="swiper-scrollbar"   slot="scrollbar"></div>
+    </swiper>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgurl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/2f5f3ea4698c9b7898db7562d89b91ed.jpg_750x200_bd3b4ce9.jpg'
+      }, {
+        id: '0002',
+        imgurl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/980adfa646fede8d7a4c46feabaac6ee.jpg_750x200_63ecd371.jpg'
+      }, {
+        id: '0003',
+        imgurl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/ed5f4115226306e48e6fad106a038afe.jpg_750x200_64df48d6.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active
+        background: #fff
+    .wrapper
+        overflow: hidden
+        width: 100%
+        height: 0
+        padding-bottom: 31.25%
+        bachground: #eee
+        .swiper-img
+            width: 100%
+</style>
